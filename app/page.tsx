3dc56ed58c2f62ea2ff3e3e0058dd17195947309
Robert Johnson <robertjohnson.NEMD@gmail.com>
2026-02-26 00:14:51 -0500

    fix?

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
   {surgeons.map((s) => (
     <li key={s.id}>
       <a
-  href={`/s/${s.id}`}
+  href={`/s?id=${s.id}`}
   target="_blank"
   rel="noreferrer"
   className="block p-4 bg-white rounded shadow border border-gray-200 hover:bg-gray-50 active:bg-gray-100 cursor-pointer"
