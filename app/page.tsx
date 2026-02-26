87e512139f872f85709ceb32a5bc699f64ddf4fa
Robert Johnson <robertjohnson.NEMD@gmail.com>
2026-02-25 23:22:57 -0500

    fix my screw up v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";href
+import { supabase } from "../lib/supabaseClient";
 import Link from "next/link";
 
 type Surgeon = {
