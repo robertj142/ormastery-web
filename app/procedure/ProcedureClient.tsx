3926c1df75acf7a846bc9a6a5914ffba6dfb4c85
Robert Johnson <robertjohnson.NEMD@gmail.com>
2026-03-03 22:38:44 -0500

    cdnter

--- app/procedure/ProcedureClient.tsx
+++ app/procedure/ProcedureClient.tsx
@@ -427,7 +427,7 @@ export default function ProcedureClient() {
 
       {/* TITLE ONLY (no extra outer box) */}
       <div className="mx-auto w-full max-w-3xl">
-        <div className="text-3xl font-black text-white tracking-tight align-center">
+        <div className="text-3xl font-black text-white tracking-tight text-center">
           {proc.name}
         </div>
 
